5c48654415a2d175150c13a97f5eee259711f2dd
kenichi04 <slow_life_1407@yahoo.co.jp>
2021-01-17 15:47:26 +0900

    整数の場合は小数点以下切り捨て

--- app/src/main/java/jp/techacademy/kenichi04/calcapp/SecondActivity.kt
+++ app/src/main/java/jp/techacademy/kenichi04/calcapp/SecondActivity.kt
@@ -9,11 +9,25 @@ class SecondActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
-        val num1 = intent.getDoubleExtra("num1", 0.0)
-        val num2 = intent.getDoubleExtra("num2", 0.0)
+        var num1 = intent.getDoubleExtra("num1", 0.0)
+        var num2 = intent.getDoubleExtra("num2", 0.0)
         val symbol = intent.getStringExtra("symbol")
-        val result = intent.getDoubleExtra("result", 0.0)
+        var result = intent.getDoubleExtra("result", 0.0)
 
-        textView.text = "${num1} ${symbol} ${num2} = ${result} です"
+        var strNum1 = num1.toString()
+        var strNum2 = num2.toString()
+        var strResult = result.toString()
+
+        if (num1.toString().endsWith(".0")) {
+            strNum1 = num1.toInt().toString()
+        }
+        if (num2.toString().endsWith(".0")) {
+            strNum2 = num2.toInt().toString()
+        }
+        if (result.toString().endsWith(".0")) {
+            strResult = result.toInt().toString()
+        }
+
+        textView.text = "${strNum1} ${symbol} ${strNum2} = ${strResult} です"
     }
 }
